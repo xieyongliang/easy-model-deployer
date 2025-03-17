126eb6acb4dd0eab045ed618860dae2ef1547e89
xieyongliang <yongliangxie@hotmail.com>
2025-03-17 14:21:01 +0800

    cleanup

--- src/pipeline/backend/ktransformers/ktransformers_backend.py
+++ src/pipeline/backend/ktransformers/ktransformers_backend.py
@@ -39,9 +39,25 @@ class KTransformersCppBackend(OpenAICompitableProxyBackendBase):
     def create_proxy_server_start_command(self,model_path):
         # find gguf from model path
         gguf_model_path = self.find_gguf_file(model_path)
-        # compute_compability: g5 - 8.6, g6 - 8.9
-        TORCH_CUDA_ARCH_LIST = '8.6' if self.instance_type.contains('g5') else '8.9'
-        cpu_infer = 94 if self.instance_type.contains('24xlarge') else 62
+
+        TORCH_CUDA_ARCH_LIST = None
+        if self.instance_type.contains('g5'):
+            TORCH_CUDA_ARCH_LIST = '8.6'
+        elif self.instance_type.contains('g6'):
+            TORCH_CUDA_ARCH_LIST = '8.9'
+        else:
+            raise ValueError(f"Unsupported instance type!")
+
+        cpu_infer = None
+        if self.instance_type.contains('24xlarge'):
+            cpu_infer = 94 
+        elif self.instance_type.contains('16xlarge') or self.instance_type.contains('12xlarge'):
+            cpu_infer = 62
+        elif self.instance_type.contains('8xlarge'):
+            cpu_infer = 30
+        else:
+            raise ValueError(f"Unsupported instance type!")
+
         max_new_tokens = 2048
         serve_command = f'TORCH_CUDA_ARCH_LIST = {TORCH_CUDA_ARCH_LIST} python /opt/ml/code/ktransformers/ktransformers/server/main.py  --model_path /opt/ml/model/DeepSeek-R1  --gguf_path {gguf_model_path} --port {self.server_port} --cpu_infer {cpu_infer} --max_new_tokens {max_new_tokens}'
         if self.environment_variables:
